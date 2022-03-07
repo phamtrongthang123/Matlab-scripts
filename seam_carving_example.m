9b53832205f35d6cfadb7cdc6d487eff7d1db551
Trong-Thang Pham <phamtrongthang123@gmail.com>
2022-03-07 15:12:55 +0700

    update lab saliency

--- seam_carving_example.m
+++ seam_carving_example.m
@@ -1,12 +1,12 @@
 img = imread('RGBD Data/food_bag_8_1_1.png');
 img_gray = rgb2gray(img);
-importance_map = importance_computing(img_gray);
+importance_map = lab_saliency(img);
 
 % figure, imshow(importance_map);
 
 vals = importance_map;
 imghere = img;
-[vals, imghere] = seamcarving(vals,imghere,50);
+[vals, imghere] = seamcarving(vals,imghere,100);
 figure, imshow(vals, []);
 figure, imshow(uint8(imghere), []);
 
